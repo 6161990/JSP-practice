fe3cc66d23cffdab519472ff4fde2d7c17314aa3
6161990 <6161990src@gmail.com>
2021-04-17 14:31:01 +0900

    CREATE MemberInfo.jsp, MemberList.jsp

--- Database/src/main/java/model/MemberDAO.java
+++ Database/src/main/java/model/MemberDAO.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Vector;
 
 //데이터 베이스에 연결하고 select, insert, update,delete 작업을 실행해주는 클래스
 public class MemberDAO {
@@ -65,5 +66,98 @@ public class MemberDAO {
 					e.printStackTrace();
 				}
 	}
+	
+	
+	//모든 회원의 정보를 리턴해주는 메소드 호출
+	public Vector<MemberBean> allSelectMember(){
+		
+		//가변길이로 데이터를 저장
+		Vector<MemberBean> v = new Vector<>();
+
+		//데이터 베이스는 무조건 예외처리를 해야한다.
+		
+		try {
+			//커넥션 연결
+			getCon();
+			//쿼리준비
+			String sql ="select * from member";
+			//쿼리를 실행시켜주는 객체 선언
+			pstmt = conn.prepareStatement(sql);
+			//쿼리를 실행시킨 결과를 리턴해서 받아줌(오라클 테이블의 검색된 결과를 자바 객체에 저장)
+			rs=pstmt.executeQuery();
+			
+			//반복문을 사용해서 rs에 저장된 데이터를 추출
+			while(rs.next()) {
+				 MemberBean bean = new MemberBean(); //컬럼으로 나뉘어진 데이터를 빈 클래스에 저장 
+				 bean.setId(rs.getString(1));
+				 bean.setPass1(rs.getString(2));
+				 bean.setEmail(rs.getString(3));
+				 bean.setTel(rs.getString(4));
+				 bean.setHobby(rs.getString(5));
+				 bean.setJob(rs.getString(6));
+				 bean.setAge(rs.getString(7));
+				 bean.setInfo(rs.getString(8));	 
+				 
+				 //패키징된 memberbean클래스를 백터에 저장 
+				 v.add(bean);//0번지 부터 순서대로 데이터가 저장
+			}
+			
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		//다 저장된 vector를 리턴
+		return v;
+	}
+	
+	
+	
+	public MemberBean oneSelectMember(String id){
+		//한사람에 대한 정보만 리턴하기에 빈클래스 객체 생성
+		MemberBean bean = new MemberBean();
+		//무조건 데이터 베이스는 예외 처리를 반드시 해야 합니다.
+		 try{
+			 //커넥션 연결 
+			 getCon();
+			 
+			 //쿼리 준비 
+			 String SQL = "SELECT *FROM MEMBER WHERE id= ?";
+			 //쿼리를 실행 시켜주는 객체 선언 
+			 pstmt = conn.prepareStatement(SQL);		 
+			 //?의 값을 맵핑 
+			 pstmt.setString(1,id);
+			 //쿼리 실행 
+			 rs = pstmt.executeQuery();
+			 if(rs.next()){//레코드가 있다면 
+				 bean.setId(rs.getString(1));
+				 bean.setPass1(rs.getString(2));
+				 bean.setEmail(rs.getString(3));
+				 bean.setTel(rs.getString(4));
+				 bean.setHobby(rs.getString(5));
+				 bean.setJob(rs.getString(6));
+				 bean.setAge(rs.getString(7));
+				 bean.setInfo(rs.getString(8));	 		 
+			 }
+			 
+			 //자원 반납
+			 conn.close();
+			 
+		    }catch(Exception e){
+		    	e.printStackTrace();
+		   	}	
+		 //리턴 
+		return bean;	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
--- Database/src/main/webapp/MemberJoinProc.jsp
+++ Database/src/main/webapp/MemberJoinProc.jsp
@@ -26,7 +26,7 @@
 		}
 	%>
 	
-	<!-- useBean을 이용하여 한꺼번에 데이터를 받아온다. -->	
+	<!-- useBean을 이용하여 한꺼번에 데이터를 사용자로부터 받아온다. 그것을 db에 넣는다(맵핑) -->	
 	<jsp:useBean id="mbean" class="model.MemberBean">
 		<jsp:setProperty name="mbean" property="*"/> 
 	</jsp:useBean>
@@ -35,10 +35,14 @@
 		//기존 취미는 주소 번지가 저장되기에 배열의 태용을 하나의 스트링으로 저장한 변수를 다시 입력
 		mbean.setHobby(texthobby); 
 	
+		//데이터 베이스 클래스 객체 생성
 		MemberDAO mdao=new MemberDAO();
-		mdao.insertMember(mbean);
+		mdao.insertMember(mbean);//데이터 베이스에 회원 정보 삽임
 		
-	
+		
+		//회원 가입이 되었다면 회원 정보를 보여주는 페이지로 이동시킴
+		response.sendRedirect("MemberList.jsp");
+		
 	 %>
 	 
 	
