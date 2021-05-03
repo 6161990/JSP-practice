45b0f6a64b58c1366addabe2063b21623cac8071
6161990 <6161990src@gmail.com>
2021-05-03 16:28:18 +0900

    MemberList clear! plus JSTL tag

--- MemberMVC/src/main/java/control/MemberJoinProc.java
+++ MemberMVC/src/main/java/control/MemberJoinProc.java
@@ -56,7 +56,7 @@ public class MemberJoinProc extends HttpServlet {
 			mdao.insertMember(bean);
 			
 			//컨트롤러에서 또 다른 컨트롤러 호출하기
-			RequestDispatcher dis = request.getRequestDispatcher("MemberListCon.do");
+			RequestDispatcher dis = request.getRequestDispatcher("/MemberListCon.do");
 			dis.forward(request, response);
 		}else {
 			request.setAttribute("msg", "패스워드가 일치하지 않습니다"); //jsp가 아니므로 request객체를 jsp쪽으로 넘겨주어야함
